6d06dd8918c45cacbcf7b3d9703a29fea083a1ab
Verinda <gen.intertsingtao@gmail.com>
2014-12-11 01:18:20 +0800

    Initial Commit

---
+++ ElectricityUsingLogOrAnalyse/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ElectricityUsingLogOrAnalyse
+//
+//  Created by Ian Wu on 14/12/11.
+//  Copyright (c) 2014年 Ian. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
